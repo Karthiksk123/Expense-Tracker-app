92e8ee1bea113cb9ddd5e2f94cd20364182a6878
Nivetha <19urcs024@aaacet.ac.in>
2023-02-08 13:15:05 +0530

    vercel changes

--- client/src/redux/api/apiSlice.js
+++ client/src/redux/api/apiSlice.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const baseURL = "http://localhost:8080";
+const baseURL = "http://127.0.0.1:8080/";
 
 export const apiSlice = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: baseURL }),
